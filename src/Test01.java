30ea2041e3550a6fa0ec9d37000367fc50e51666
afan95 <52150286+afan95@users.noreply.github.com>
2024-04-12 22:28:47 +0800

    测试一下

---
+++ src/Test01.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: afan
+ * @create: 2024/4/12 22:25
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
